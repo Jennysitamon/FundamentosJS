a6415461f36ca7e2a84781101216590ac4b3d01b
jeny <jenymontiel13@gmail.com>
2023-09-11 15:58:34 -0600

    05 Metodos y propiedades strings

--- operadores.js
+++ operadores.js
@@ -2,14 +2,13 @@
  
  
  // Operadores Matematicos
- // + - * / % **
+ // + - * / % 
  // += -= *= /= %= 
  console.log(10 + 10);
  console.log(10 - 10);
  console.log(10 / 10);
  console.log(10 * 10);
  console.log(10 % 10);
- console.log(10 ** 10); // Potencia
 
  let num = 15;
  console.log(num += 5); //num = num +5
@@ -17,7 +16,7 @@
  console.log(num *= 5);
  console.log(num /= 5);
  console.log(num %= 5);
- console.log(num **= 5);
+
  //Operadores booleanos
  // > < >= <= !== ===
  console.log(1 > 2);
@@ -26,6 +25,14 @@
  console.log(1 <= 2);
  console.log(1 !== 2);
  console.log(1 === 2);
+ 
+ //Operadores logicos
+ // and &&
+console.log(true && true);
+ //or ||
+console.log(true || false);
+// not !
+console.log(!true);
 
 
  
---
+++ strings2.js
@@ -0,0 +1,10 @@
+// Métodos y propiedades Strings
+const text = 'Este es un T4xt0 cooL';
+console.log(text.length);
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+console.log(text.charAt(2));
+console.log(text.indexOf('T'));
+console.log(text.startsWith('T'));
+console.log(text.endsWith('E'));
+console.log('*'.repeat(10));
