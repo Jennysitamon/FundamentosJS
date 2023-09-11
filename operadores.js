295f9240071b9c35b945f8f6388de5b1bda7ec40
jeny <jenymontiel13@gmail.com>
2023-09-11 15:46:22 -0600

    04 Operadores

---
+++ operadores.js
@@ -0,0 +1,31 @@
+ 
+ 
+ 
+ // Operadores Matematicos
+ // + - * / % **
+ // += -= *= /= %= 
+ console.log(10 + 10);
+ console.log(10 - 10);
+ console.log(10 / 10);
+ console.log(10 * 10);
+ console.log(10 % 10);
+ console.log(10 ** 10); // Potencia
+
+ let num = 15;
+ console.log(num += 5); //num = num +5
+ console.log(num -= 5);
+ console.log(num *= 5);
+ console.log(num /= 5);
+ console.log(num %= 5);
+ console.log(num **= 5);
+ //Operadores booleanos
+ // > < >= <= !== ===
+ console.log(1 > 2);
+ console.log(1 < 2);
+ console.log(1 >= 2);
+ console.log(1 <= 2);
+ console.log(1 !== 2);
+ console.log(1 === 2);
+
+
+ 
