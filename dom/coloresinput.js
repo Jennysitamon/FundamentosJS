2ec411494efd48d1d2a8d1f85dc74c5d966193ed
jeny <jenymontiel13@gmail.com>
2023-09-26 10:14:41 -0600

    16 DOM Galeria Bootstrap

--- dom/coloresinput.js
+++ dom/coloresinput.js
@@ -7,7 +7,7 @@ function getColor() {
     const pinkVal = Math.floor((pink.value / 100) * 255);
     const purpleVal = Math.floor((purple.value / 100) * 255);
     const goldenVal = Math.floor((golden.value / 100) * 255);
-    const alpha = pink.value / 100;  // Valor de transparencia entre 0 y 1
+    const alpha = pink.value / 100; 
     return `rgba(${pinkVal}, ${purpleVal}, ${goldenVal}, ${alpha})`;
 }
 
@@ -17,6 +17,6 @@ function setColor() {
 
 setColor();
 
-pink.addEventListener('input', setColor);  // Cambiado 'change' a 'input'
-purple.addEventListener('input', setColor);  // Cambiado 'change' a 'input'
-golden.addEventListener('input', setColor);  // Cambiado 'change' a 'input'
+pink.addEventListener('input', setColor);  
+purple.addEventListener('input', setColor);  
+golden.addEventListener('input', setColor);  
